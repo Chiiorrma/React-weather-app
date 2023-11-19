a612e8b253ca932e34a97c5617c28530ed039d68
Chiiorrma <chiumuo76@gmail.com>
2023-11-19 08:15:04 -0800

    Fixed the errors

--- my-weather-app/src/WeatherInfo.js
+++ my-weather-app/src/WeatherInfo.js
@@ -6,14 +6,14 @@ import WeatherIcon from "./WeatherIcon";
 export default function WeatherInfo(props) {
 
   return (
-    <div>
-      <h1 class="mt-4">{props.data.city}</h1>
+    <div className="WeatherInfo">
+      <h1 className="mt-4">{props.data.city}</h1>
       <div>
         <FormattedDate date={props.data.date} />
       </div>
       <div className="text-capitalize">{props.data.description}</div>
-      <div class="row mt-3">
-        <div class="col-6">
+      <div className="row mt-3">
+        <div className="col-6">
           <div className="clear-fix">
             <div className="float-left">
               <WeatherIcon code={props.data.icon} />
@@ -23,12 +23,13 @@ export default function WeatherInfo(props) {
             </div>
           </div>
         </div>
-      </div>
-      <div class="col-md-6">
-        <ul>
-          <li>Humidity:{props.data.humidity}%</li>
-          <li>Wind:{props.data.wind} km/h</li>
-        </ul>
+
+        <div className="col-md-6">
+          <ul>
+            <li>Humidity:{props.data.humidity}%</li>
+            <li>Wind:{props.data.wind} km/h</li>
+          </ul>
+        </div>
       </div>
     </div>
   )
