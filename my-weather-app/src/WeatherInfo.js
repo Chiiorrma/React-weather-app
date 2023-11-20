08caeacb68a1275d5a403e7677cf4f879e838143
Chiiorrma <chiumuo76@gmail.com>
2023-11-20 12:37:32 -0800

    Updated

--- my-weather-app/src/WeatherInfo.js
+++ my-weather-app/src/WeatherInfo.js
@@ -14,7 +14,7 @@ export default function WeatherInfo(props) {
       <div className="text-capitalize">{props.data.description}</div>
       <div className="row mt-3">
         <div className="col-6">
-          <div >
+          <div className="d-flex">
             <div >
               <WeatherIcon code={props.data.icon} size={52} />
             </div>
