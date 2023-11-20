200191dfadae642ea06fe6e3f73254e392208f87
Chiiorrma <chiumuo76@gmail.com>
2023-11-20 12:03:25 -0800

    Removed functions for changing temperature unit

--- my-weather-app/src/WeatherTemperature.js
+++ my-weather-app/src/WeatherTemperature.js
@@ -1,37 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("celsuis");
-  function displayFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-  function displayCelsuis(event) {
-    event.preventDefault();
-    setUnit("celsuis");
-  }
-  function farenheit() {
-    return (props.celsuis * 9) / 5 + 32;
-  }
-  if (unit === "celsuis") {
-    return (
-      <div>
-        <span className="temperature "> {Math.round(props.celsuis)} </span>
-        <span className="unit ">°C |
-          <a href="/" onClick={displayFahrenheit}>°F</a>
-        </span>
-      </div >
-    )
-  }
-  else {
-
-    return (
-      <div>
-        <span className="temperature "> {Math.round(farenheit())} </span>
-        <span className="unit "><a href="/" onClick={displayCelsuis}>°C |</a> °F
-        </span>
-      </div>
-    )
-  }
+  return (
+    <div className="WeatherTemperature">
+      <span className="temperature "> {Math.round(props.celsuis)} </span>
+      <span className="unit ">°C </span>
+    </div >
+  )
 }
